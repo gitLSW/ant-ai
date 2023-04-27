5dfbbc7a758134c14c8996f86309ee1084f9ad98
Laurenz Weckerle <ge93bok@mytum.de>
2023-04-27 16:42:48 +0200

    Tried adding an AI system

--- systems/ai-control.js
+++ systems/ai-control.js
@@ -1,12 +1,33 @@
-export default (entities, { touches }) => {
-    // From this we can calculate the position of each object for the ant
-    // console.log(Object.values(entities).map(val => val?.body?.position))
-    
-    // const ants = Object.entries(entities).filter(entity => entity[0].startsWith('Ant'))
+import Matter from "matter-js"
 
-    // for (const ant of ants) {
-    //     const sightVector = 
-    // }
+export default (entities, { touches }) => {
+    const ants = Object.entries(entities).filter(entity => entity[0].startsWith('Ant')).map(entry => entry[1])
+    const spiders = Object.entries(entities).filter(entity => entity[0].startsWith('Spider')).map(entry => entry[1])
+    const ressources = Object.entries(entities).filter(entity => entity[0].startsWith('Ressource')).map(entry => entry[1])
+
+    const obstacles = Object.entries(entities).filter(entity => entity[0].startsWith('Spider')).map(entry => entry[1])
+
+    for (const ant of ants) {
+        console.log(ant)
+
+        const destinations = ressources
+            .map(ressource => {
+                const direction = {
+                    x: ant.body.position.x - ressource.body.position.x,
+                    y: ant.body.position.y - ressource.body.position.y
+                }
+
+                const distance = Math.sqrt(Math.pow(direction.x, 2) + Math.pow(direction.y, 2))
+
+                return { distance, dir: direction }
+            })
+
+        const destination = destinations.reduce((min, current) => {
+            return current.distance < min.distance ? current : min;
+        }, destinations[0])
+
+        Matter.Body.setVelocity(ant.body, destination.dir)
+    }
 
     return entities;
 }
