559566925045c1ad6ea6f2713c45f82fc7301765
Heragoga <heragogaa1@gmail.com>
2023-06-17 22:03:16 +0200

    Added Border

--- entities/Border.js
+++ entities/Border.js
@@ -9,65 +9,36 @@ const Border = props => {
     const xBody = props.body.position.x - widthBody / 2
     const yBody = props.body.position.y - heightBody / 2
 
-    // const color = props.color;
-
-    function getRandom(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
-    // PATH THAT CREATES A RANDOM POLYGON WITH A SINGLE LARGE HOLE IN THE MIDDLE:
-    // e.g. Random Point Generation in 40px rectangle
-    var polygonPoints = [{ x: 0, y: 0 }]
-    for (let index = 1; index <= 10; index++) {
-        polygonPoints.push({
-            x: getRandom(0, widthBody * 0.15),
-            y: getRandom(0, (heightBody * index) / 10)
-        })
-    }
-
-    polygonPoints.push({ x: widthBody, y: 0 })
-    for (let index = 1; index <= 10; index++) {
-    }
-
-    polygonPoints.push({ x: 0, y: heightBody })
-    for (let index = 1; index <= 10; index++) {
-    }
-
-    polygonPoints.push({ x: widthBody, y: heightBody })
-    for (let index = 1; index <= 10; index++) {
-    }
-
     return (
         <View
-            style={{
-                background: 'grey',
-                position: 'absolute',
-                left: xBody,
-                top: yBody,
-                width: widthBody,
-                height: heightBody
-            }} />
+            
+        style={{
+            background: 'grey',
+            position: 'absolute',
+            left: xBody,
+            top: yBody,
+            width: widthBody,
+            height: heightBody
+        }} />
     )
 }
 
 export default (world, pos, size) => {
-    const initial = Matter.Bodies.rectangle(
+    const inital = Matter.Bodies.rectangle(
         pos.x,
         pos.y,
         size.width,
         size.height,
         {
-            label: 'Border',
+            label: `Border`,
             isStatic: true
         }
     )
 
-    Matter.World.add(world, initial)
+    Matter.World.add(world, inital)
 
     return {
-        body: initial,
+        body: inital,
         pos,
         renderer: <Border />
     }
