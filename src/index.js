e567fde8bbf042c3bc16e227c74f1fecea5f8547
Laurenz Weckerle <ge93bok@mytum.de>
2023-06-24 01:22:19 +0200

    Added Images

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
-const { QMainWindow, QWidget, FlexLayout } = require("@nodegui/nodegui")
+const { QMainWindow, QLabel, QPixmap } = require("@nodegui/nodegui")
+const path = require('path')
 const populateField = require('./populateField')
-// const logo = require('../assets/ant.png')
 
-const worldSize = { width: 500, height: 500 }
+const floorTileSize = 50
+const worldTileNum = 10
+const worldSize = { width: floorTileSize * worldTileNum, height: floorTileSize * worldTileNum }
 
 const win = new QMainWindow()
 win.setFixedSize(worldSize.width, worldSize.height)
 
 // Set Background
+var floorImg = new QPixmap()
+const absPath = path.resolve(`${__dirname}/../assets/floor.png`).toString()
+floorImg.load(absPath)
+floorImg = floorImg.scaled(floorTileSize, floorTileSize)
+
+for (let i = 0; i < worldTileNum; i++) {
+    for (let j = 0; j < worldTileNum; j++) {
+        const label = new QLabel(win)
+        label.setObjectName(`floor_${i}_${j}`)
+        label.setGeometry(i * floorTileSize, j * floorTileSize, floorTileSize, floorTileSize)
+        label.setPixmap(floorImg)
+    }
+}
 
 populateField(win, worldSize)
 
@@ -15,33 +30,35 @@ win.show()
 global.win = win
 
 // Game RunLoop
-const speed = 5
-var dir = 1
+// const speed = 5
+// var dir = 1
 
-function gameLoop() {
-    try {
-        win.children().forEach(child => {
-            if (!child) {
-                return;
-            }
+// const intervalId = setInterval(gameLoop, 30);
 
-            const id = child.objectName();
-            const type = id.split('_')[0];
-            const pos = child.pos();
+// function gameLoop() {
+//     try {
+//         win.children().forEach(child => {
+//             if (!child) {
+//                 return;
+//             }
 
-            if (worldSize.height < pos.y) {
-                dir = -1;
-            } else if (pos.y < 0) {
-                dir = 1;
-            }
+//             const id = child.objectName();
+//             const type = id.split('_')[0];
+//             const pos = child.pos();
 
-            if (type === 'Ant' || type === 'Spider') {
-                child.move(pos.x, pos.y + dir * speed);
-            }
-        });
-    } catch (e) {
-        console.log(e)
-    }
-}
+//             if (worldSize.height < pos.y) {
+//                 dir = -1;
+//             } else if (pos.y < 0) {
+//                 dir = 1;
+//             }
 
-const intervalId = setInterval(gameLoop, 30);
+//             if (type === 'Ant' || type === 'Spider') {
+//                 child.move(pos.x, pos.y + dir * speed);
+//             }
+//         });
+//     } catch (e) {
+//         console.log(e)
+//         clearInterval(intervalId)
+//         return
+//     }
+// }
--- src/populateField.js
+++ src/populateField.js
@@ -1,43 +1,72 @@
-const { QWidget } = require("@nodegui/nodegui")
+const { QLabel, QPixmap, QWidget } = require("@nodegui/nodegui")
+const path = require('path')
+
+var imageCache = {}
+
+function getImage(imageName, size) {
+    if (imageCache[imageName]) {
+        return imageCache[imageName]
+    }
+
+    var image = new QPixmap()
+    const absPath = path.resolve(`${__dirname}/../assets/${imageName}.png`).toString()
+    image.load(absPath)
+    image = image.scaled(size.width, size.height, 1)
+
+    imageCache[imageName] = image
+
+    return image
+}
+
+function GameObj(imageName, objName, pos, size, parent) {
+    const label = new QLabel(parent)
+    label.setObjectName(objName)
+    label.setGeometry(pos.x, pos.y, size.width, size.height)
+    
+    label.setPixmap(getImage(imageName, size))
+
+    return label
+}
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function getRandomCoordiante(size) {
+    return { x: getRandom(0, size.width), y: getRandom(0, size.height) }
+}
+
+function getObstacleImage() {
+    switch (getRandom(0, 2)) {
+        case 0:
+            return 'stone'
+        case 1:
+            return 'leaf'
+        case 2:
+            return 'twig'
+    }
+}
 
 function populateField(win, worldSize) {
-    const ressourceSize = { width: 7, height: 7 }
-    const obstacleSize = { width: 15, height: 15 }
-    const spiderSize = { width: 10, height: 10 }
-    const antSize = { width: 5, height: 5 }
+    const ressourceSize = { width: 30, height: 30 }
+    const obstacleSize = { width: 35, height: 35 }
+    const spiderSize = { width: 25, height: 25 }
+    const antSize = { width: 15, height: 15 }
 
-    const ant = new QWidget(win)
-    ant.setObjectName('Ant_0')
-    ant.setGeometry(worldSize.width / 2, worldSize.height / 2, antSize.width, antSize.height)
-    ant.setInlineStyle("background-color: blue;")
-
-    function getRandom(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min)
-    }
-
-    function getRandomCoordiante(size) {
-        return { x: getRandom(0, size.width), y: getRandom(0, size.height) }
-    }
+    const ant = GameObj('ant', 'Ant_0', { x: worldSize.width / 2, y: worldSize.height / 2 }, antSize, win)
 
     for (let i = 0; i < 20; i++) {
-        const gameObj = new QWidget(win)
         const randCoordinate = getRandomCoordiante(worldSize)
 
         let random = getRandom(0, 4)
         if (0 <= random && random <= 2) {
-            gameObj.setObjectName('Obstacle_' + i)
-            gameObj.setGeometry(randCoordinate.x, randCoordinate.y, obstacleSize.width, obstacleSize.height)
-            gameObj.setInlineStyle("background-color: grey;")
+            const obstacle = new GameObj(getObstacleImage(), `Obstacle_${i}`, randCoordinate, obstacleSize, win)
         }
         else if (random == 3) {
-            gameObj.setObjectName('Spider_' + i)
-            gameObj.setGeometry(randCoordinate.x, randCoordinate.y, spiderSize.width, spiderSize.height)
-            gameObj.setInlineStyle("background-color: red;")
+            const spider = new GameObj('spider', `Spider_${i}`, randCoordinate, spiderSize, win)
         }
         else if (random == 4) {
-            gameObj.setObjectName('Ressource_' + i)
-            gameObj.setGeometry(randCoordinate.x, randCoordinate.y, ressourceSize.width, ressourceSize.height)
-            gameObj.setInlineStyle("background-color: yellow;")
+            const ressource = new GameObj('berries', `Ressource_${i}`, randCoordinate, ressourceSize, win)
         }
     }
 }
